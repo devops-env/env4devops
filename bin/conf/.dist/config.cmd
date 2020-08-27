88bc08cfd2f83283ec130936cca0a25bc937f6ae
wuding <netjoin@gmail.com>
2020-08-27 23:57:02 +0800

    新增配置分发文件

---
+++ bin/conf/.dist/config.cmd
@@ -0,0 +1,14 @@
+SET soft_dir=Q:\System\ProgramFiles
+
+SET php_call=
+SET php_v=7.4
+SET php_ver=%php_v%.9-nts
+
+SET php_dir=%soft_dir%\php-%php_ver%
+SET php_ini=%php_dir%\php-.ini
+
+SET php_addr=0.0.0.0
+SET php_port=8080
+SET php_listen=
+SET php_docroot=N:\Server\VCS\GitHub\wuding\php-app\web
+SET php_router=%php_docroot%\router.php
